1051393c539585d34f47857b9710f430a30c16b2
XivC <57796626+XivC@users.noreply.github.com>
2020-11-09 08:34:34 +0300

    Pokemon lib fix

--- compile.bat
+++ compile.bat
@@ -1,4 +1,4 @@
 javac -target 8 -source 8 -cp ;lib/Pokemon.jar com/lab2/Lab2.java com/lab2/customs/pokemons/*.java com/lab2/customs/attacks/*.java com/lab2/battlegrounds/*.java
-jar cfm lab2.jar MANIFEST.MF com lib
+jar cfm lab2.jar MANIFEST.MF com/lab2/*.class com/lab2/battlegrounds/*.class com/lab2/customs/attacks/*.class com/lab2/customs/pokemons/*.class lib
 java -jar lab2.jar
 pause
